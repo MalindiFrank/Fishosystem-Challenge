24a2282b4ffdbd4fce5e57c51a90541956695029
kamogotjhb024 <kamogotjhb024@student.wethinkcode.co.za>
2025-06-23 23:48:15 +0200

    cleaned notifier func in fisho.py

--- fisho.py
+++ fisho.py
@@ -15,18 +15,17 @@ fishosys = [[random.choices([" ", ">", "#"], [25, 2, 1])[0] for i in range(10)] 
 
 def fish_noticer(fishosys = fishosys):
 
-    for y, row in enumerate(fishosys):
-        for x, object in enumerate(row):
-            if object == ">":
-                return x, y
-
+    for row_index, row in enumerate(fishosys):
+        for col_index, cell in enumerate(row):
+            if cell == ">":
+                return row_index, col_index
 
 def shark_noticer(fishosys = fishosys):
 
-    for y, row in enumerate(fishosys):
-        for x, object in enumerate(row):
-            if object == "#":
-                return x, y
+    for row_index, row in enumerate(fishosys):
+        for col_index, cell in enumerate(row):
+            if cell == ">":
+                return row_index, col_index
             
 def space(fishosys = fishosys):
 
